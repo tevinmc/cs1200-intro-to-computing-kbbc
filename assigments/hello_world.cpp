746a4fe0a7c44fd955441f170d1467c8b20f40eb
Tevin McQuilkin <tevin@sharedstudios.com>
2020-10-02 10:03:40 -0400

    hello word completed

--- assigments/hello_world.cpp
+++ assigments/hello_world.cpp
@@ -2,7 +2,7 @@
     Name: Hello World
     Copyright: 2020
     Author: Tevin McQuilkin
-    Date: Sat Sep 19 2020
+    Date: Mon Sep 21 2020
     Description: This program turn a string into string buffer thats streamed into the iostream 
                   cout object displays the buffered string data(chars) over standard output.
  */
@@ -12,7 +12,7 @@ using namespace std;
 
 int main (){
 
-	cout << "Hello World" << endl;
+	cout << "Hello World\n";
     
 	return 0;
 }
