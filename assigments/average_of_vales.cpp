2ee55c5940659199615259ba3b01a563f5280c4d
Tevin McQuilkin <tevin@sharedstudios.com>
2020-10-02 10:03:29 -0400

    average assigment completed

--- assigments/average_of_vales.cpp
+++ assigments/average_of_vales.cpp
@@ -1,32 +1,29 @@
- /* 
-    Name: Average of Values
+/* 
+    Name: average_of_vales
     Copyright: MIT 2020
     Author: Tevin McQuilkin
-    Date: Sun Sep 20 2020
-    Description: This program assigns a series of variable to representing a range of numbers.
-                 Gathers the sum of the ranage and divides that sum by size of the the range to 
-                 get the average. Finally it displayed the outputs over stanadard out.
+    Date: Sat Sep 26 2020
+    Description: This is a program that calculates the average of a set of five numbers and output that average to the console over standard output.
  */
-
  
-  #include <iostream>        
+  #include <iostream>
+  #include <iomanip>               
   using namespace std;
 
   int main (){
 
-    // Variable declarations
-    int num1 = 28, num2 = 32, num3 = 37, num4 = 24, num5 = 33;
+    // Declaration of variables for the set of numbers, the sum of those numbers and the average of the set.
+    double num1 = 28, num2 = 32, num3 = 37, num4 = 24, num5 = 33;
 
-    // Calculates the sum
-    int sum = num1 + num2 + num3 + num4 + num5;
+    double sum = num1 + num2 + num3 + num4 + num5;
 
-    // Calculates the average
-    float average = (float) sum / 5; 
+    double average =  sum / 5; 
 
-    // Output values
-    cout << "Average\n" << average << endl;
-    
+    // Output statement of the computed average of the set of values 
+    cout << "The average of the set of numbers 28,32,37,24,33 : " << average << endl;
     return 0;
   }
 
+ 
+ 
  
